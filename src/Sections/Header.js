2dd3acf3cfa2518364b8896461a2f7f2c9a3adad
marcelo1310 <marcelotestes9010@gmail.com>
2025-09-20 00:50:58 -0300

    atualização

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -1,3 +1,4 @@
+import { Botao } from "../Components/Botao/Botao";
 export function Header() {
   const header = document.createElement("header");
   header.innerHTML = `
@@ -7,35 +8,42 @@ export function Header() {
       <button class="menu-toggle" aria-label="Abrir Menu">☰</button>
       <nav class="header-nav">
         <ul>
-          <li><a class="header-link" href="#">Início</a></li>
-          <li><a class="header-link" href="#">Sobre</a></li>
-          <li><a class="header-link" href="#">Projetos</a></li>
-          <li><a class="header-link" href="#">Contato</a></li>
+          <li><a class="header-link" href="#hero">Início</a></li>
+          <li><a class="header-link" href="#sobre">Sobre</a></li>
+          <li><a class="header-link" href="#projetos">Projetos</a></li>
+          <li><a class="header-link" href="#contato">Contato</a></li>
         </ul>
+        <div class="btn-header"></div>
       </nav>
     </div>
   </div>
     `;
 
-  const toggle = header.querySelector(".menu-toggle", ".header-link");
+  const toggle = header.querySelector(".menu-toggle");
   const menu = header.querySelector(".header-nav");
+  const links = header.querySelectorAll(".header-link");
 
   toggle.addEventListener("click", () => {
     menu.classList.toggle("show");
   });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const headerscroll = document.getElementById('header');
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-      headerscroll.classList.add('scrolled');
-    } else {
-      headerscroll.classList.remove('scrolled');
-    }
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("show");
+    });
   });
-});
 
+  document.addEventListener("DOMContentLoaded", () => {
+    const headerscroll = document.getElementById("header");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        headerscroll.classList.add("scrolled");
+      } else {
+        headerscroll.classList.remove("scrolled");
+      }
+    });
+  });
 
   return header;
 }
